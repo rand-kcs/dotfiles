51257821fc20ba54b53f5c8b9f92ece723912a18
Huang <2460969945@qq.com>
2024-11-06 02:07:01 -0800

    make auto install on tmux and zsh

--- autoconfig.sh
+++ autoconfig.sh
@@ -8,10 +8,19 @@
 # 2. download tmux if not installed
 # 3. auto config 'auto suggestion' and 'auto smart jump'
 # 4. git ssh?
+# clash config : lanqiaoyun
+# downloading need files for plugin in dir 'plugin'
 
 files=$(ls -a | grep -E '^\.[^.]' | grep -v '.git$' )
 
-sudo apt-get install tmux
+
+if which zsh; then
+  sudo apt-get install tmux
+fi
+
+if which tmux; then
+  sudo apt-get install tmux
+fi
 
 for file in $files ;do
 	if [[ -e ~/$file ]]; then
